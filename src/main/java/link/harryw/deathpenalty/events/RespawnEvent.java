a821719951fdc7a365ec34081c4c13b295202f4e
AfkF24 <12214250+AfkF24@users.noreply.github.com>
2023-02-17 11:17:01 +0100

    Change event type to respawn and add a minimum balance config option (#1)
    * Change event type to respawn
    
    No functionality changes, only useful because it's probably better/more convenient to display the chat message once the player actually respawns
    
    * Improve config comments
    
    * Add minimum balance config option

---
+++ src/main/java/link/harryw/deathpenalty/events/RespawnEvent.java
@@ -0,0 +1,43 @@
+package link.harryw.deathpenalty.events;
+
+import link.harryw.deathpenalty.DeathPenalty;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+public class RespawnEvent implements Listener {
+
+    private final Economy econ;
+    private final DeathPenalty plugin;
+
+    public RespawnEvent(DeathPenalty plugin) {
+        this.plugin = plugin;
+        this.econ = DeathPenalty.econ;
+    }
+
+    @EventHandler
+    public void respawnEvent(PlayerRespawnEvent e) {
+        Player p = e.getPlayer();
+        FileConfiguration config = plugin.getConfig();
+
+        double currentBalance = econ.getBalance(p);
+        double withdrawAmount = 0.0f;
+
+        if(currentBalance >= config.getInt("minimumBalance")) {
+            if(config.getString("lossType").equalsIgnoreCase("percentage")) {
+                int percentageAmount = config.getInt("percentageLost");
+                withdrawAmount = currentBalance * (percentageAmount / 100.0f);
+            } else if(config.getString("lossType").equalsIgnoreCase("fixed")) {
+                withdrawAmount = config.getInt("amountLost");
+            }
+
+            econ.withdrawPlayer(p, withdrawAmount);
+
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("prefix")) + ChatColor.translateAlternateColorCodes('&', config.getString("deathMessage")).replace("%amount%", String.valueOf((int) withdrawAmount)));
+        }
+    }
+}
